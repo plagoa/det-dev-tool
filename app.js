4e1c40767145a2992b72d6bc5874724741fadaaa
Francisco Guilherme <fguilherme@pentaho.com>
2016-08-11 23:45:01 +0100

    Adds support for POST requests

--- app.js
+++ app.js
@@ -1,42 +1,18 @@
-var express   = require('express');
-var request   = require('request');
-var fs        = require('fs');
-var mime      = require('mime-types');
-var colors    = require('colors');
-var config    = require('./config.json');
-var proxy     = require('./proxy');
-var app       = express();
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var proxy       = require('./proxy');
+var request     = require('request');
 
-var port      = process.env.PORT || 3000;
-var baseUrl   = 'http://' + config.host + ':' + config.port;
+var port        = process.env.PORT || 3000;
 
-app.get("/*", function(req, res){
+var app         = express();
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
-  var file = proxy.parseURL(req.url);
-  load(file, res);
+app.all("/*", function(req, res){
+  proxy.proxyRequest(req, res);
 })
 
 app.listen(port, function(){
   console.log('App listening on port ' + port + '!');
 });
-
-function load(file, res) {
-
-  res.setHeader('content-type', mime.lookup(file.src));
-
-  if (file.type == 'fs') {
-    res.send(fs.readFileSync(file.src));
-    console.log(colors.green("PROXY  >", file.src));
-  }
-
-  if (file.type == 'url') {
-    request({
-      url: baseUrl + file.src,
-      method: 'GET',
-    }, function (err, response, body) {
-      if(err) return console.log(err);
-      res.send(body);
-    })
-    console.log(colors.cyan("URL    >", baseUrl + file.src));
-  }
-}
--- proxy.js
+++ proxy.js
@@ -1,31 +1,80 @@
-var _         = require('underscore');
-var request   = require('request');
-var fs        = require('fs');
-var mime      = require('mime-types');
-var config    = require('./config.json');
+var _           = require('underscore');
+var fs          = require('fs');
+var config      = require('./config.json');
+var mime        = require('mime-types');
+var request     = require('request');
+var colors      = require('colors');
+var Client      = require('node-rest-client').Client;
 
-var exceptions = [/index.html/, /\.(jpe?g|png|gif|bmp|svg)$/i]
+var config      = require('./config.json');
+var exceptions  = [/index.html/, /\.(jpe?g|png|gif|bmp|svg)$/i];
+var baseURL     = 'http://' + config.host + ':' + config.port;
+var client      = new Client();
+
 
 module.exports = {
-  parseURL: parseURL
+  proxyRequest: proxyRequest
 }
 
-function parseURL(url) {
+function proxyRequest(req, res) {
 
-  var file = {src: url, type: 'url'};
-  var path = url.split('/').slice(3, this.length).join('/');
+  switch (req.method) {
+    case "POST":
+      post(req, res);
+      break;
+    case "GET":
+      get(req, res);
+      break;
+  }
+}
+
+function post(req, res) {
+
+  client.post(baseURL + req.url, {
+    data: req.body,
+    headers: { "Content-Type": "application/json" }
+  }, function (data, response) {
+    console.log(colors.yellow("POST   >", req.url));
+    res.send(data);
+  });
+}
+
+function get(req, res) {
+
+  var url = matchURL(req.url);
+
+  res.setHeader('content-type', mime.lookup(url));
+
+  if (url.match(/http/)) {
+    console.log(colors.cyan("URL    >", url));
+    req.pipe(request(url)).pipe(res);
+  } else {
+    console.log(colors.green("PROXY  >", url));
+    res.send(fs.readFileSync(url));
+  }
+}
+
+function matchURL(url) {
+
+  var mURL = baseURL + url;
 
   _.each(config.resources, function(resource){
 
-    var isException = false;
-    _.each(exceptions, function(exception){
-      if(url.match(exception)) isException = true;
-    })
-
-    if(url.match(resource.match) && !isException) {
-      file = {src: resource.project + resource.path + path, type: 'fs'};
+    if(url.match(resource.match) && !isException(mURL)) {
+      mURL = resource.target + resource.path + url.split('/').slice(3, this.length).join('/');
     }
   });
 
-  return file;
+  return mURL;
+}
+
+function isException(url) {
+
+  var isException;
+
+  _.each(exceptions, function(exception){
+    if(url.match(exception)) isException = true;
+  })
+
+  return isException;
 }
